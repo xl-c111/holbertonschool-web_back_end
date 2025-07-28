1aeeae0415ba1d0524461bdb3f2740bccde6e997
xl-c111 <10430@holbertonstudents.com>
2025-07-29 01:57:43 +1000

    Remove 1-main.py and 1-concat.py

--- 1-concat.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-This module defines a function `concat` that concatenates two strings.
-"""
-
-
-def concat(str1: str, str2: str) -> str:
-    """Concatenates two input strings and returns the result."""
-    return (str1 + str2)
