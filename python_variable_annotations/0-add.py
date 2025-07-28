1c04a12e1fbe91c914c5828900ecaa66f23fec4b
xl-c111 <10430@holbertonstudents.com>
2025-07-29 01:03:44 +1000

    Basic annotations - concat

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-This module defines a simple function `add` that adds two integers.
+This module defines a simple function `add` that adds two floats.
 """
 
 
 def add(a: float, b: float) -> float:
+    """Add function"""
     return a + b
