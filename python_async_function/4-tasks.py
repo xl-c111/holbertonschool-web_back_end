30615df9592874c3b743b5d1da8989094f01d828
xl-c111 <10430@holbertonstudents.com>
2025-07-30 03:36:35 +1000

    Fix output - list ordered in ascending order

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Module that defines an asynchronous routine for launching multiple
-scheduled tasks concurrently using asyncio.gather.
+scheduled tasks concurrently using asyncio.as_completed.
 """
 import asyncio
 import random
@@ -14,11 +14,14 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     Run task_wait_random n times concurrently and return a list of results
-    using asyncio.gather.
+    in the order of completion using asyncio.as_completed.
     """
     tasks = []
     for _ in range(n):
         tasks.append(task_wait_random(max_delay))
 
-    results = await asyncio.gather(*tasks)
+    results = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        results.append(result)
     return results
