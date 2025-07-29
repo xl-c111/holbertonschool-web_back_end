6585435d11d356d674ef8f3051deebd0d83ed50c
xl-c111 <10430@holbertonstudents.com>
2025-07-30 05:50:45 +1000

    Change return type to Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ random float numbers between 0 and 10, with a 1-second delay between each.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously generate 10 random float numbers between 0 and 10.
     """
