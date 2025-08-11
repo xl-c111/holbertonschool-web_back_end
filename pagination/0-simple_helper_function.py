efd6d9e600462c8db4b68b2acb49eebce753ba83
xl-c111 <10430@holbertonstudents.com>
2025-08-11 23:33:11 +1000

    Add comments

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,5 +6,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Calculate the start and end index for pagination."""
     start = (page - 1) * page_size
-    end = start + page_size
+
+    # even if end > len(data), python slicing will stop at len(data)
+    end = start + page_size    # or: end = page * page_size
     return start, end
