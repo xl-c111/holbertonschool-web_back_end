f68f183a27075077cd833424b59dc6a9f446a668
xl-c111 <10430@holbertonstudents.com>
2025-08-11 19:10:37 +1000

    Add parameter and return type annotations to index_range

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Module for calculating pagination start and end indexes."""
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Calculate the start and end index for pagination."""
     start = (page - 1) * page_size
     end = start + page_size
