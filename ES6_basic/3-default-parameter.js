fc6cb5e9ac1aa20679241dc1b0cd56285a54d8b1
xl-c111 <10430@holbertonstudents.com>
2025-08-20 16:07:41 +1000

    Correct output

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,1 +1,1 @@
-export default (a = 34, b = 89, c = 19) => a + b + c;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) { return initialNumber + expansion1989 + expansion2019; }
