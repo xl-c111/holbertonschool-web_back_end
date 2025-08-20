b2c548aeb569d2c270a366445d06f829c29d4b1e
xl-c111 <10430@holbertonstudents.com>
2025-08-20 15:47:35 +1000

    Fix error - condense the function in 1 line

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,1 +1,2 @@
-export default function getSumOfHoods(initialNumber = 34, expansion1989 = 89, expansion2019 = 19) {return initialNumber + expansion1989 + expansion2019;}
+// prettier-ignore
+export default (initialNumber = 34, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
