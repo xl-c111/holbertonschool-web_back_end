cc3f16438ab748aa63369e1deaddb7b2bd70f9cc
xl-c111 <10430@holbertonstudents.com>
2025-08-23 21:02:47 +1000

    Add comments

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -5,3 +5,11 @@ export default function appendToEachArrayValue(array, appendString) {
   }
   return newArray;
 }
+
+
+// Using for...in
+/*
+for (const idx in array) {
+  array[idx] = appendString + array[idx];
+}
+*/
