f27ec13aa058dc80c80ef016980b3b09d69e4518
xl-c111 <10430@holbertonstudents.com>
2025-08-23 22:27:39 +1000

    Add comments

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,15 @@
 export default function createReportObject(employeesList) {
+  // build the report obj with two properties
+  // one is data property whose value is another obj(the shallow copy of employeesList)
   const report = {
     allEmployees: { ...employeesList },
+    // one is method property whose value is a function, this function takes one parameter named allEmployees, it returns the number of own enumerable string keys on that obj
     getNumberOfDepartments(allEmployees) {
       return Object.keys(allEmployees).length;
-    },
+    }
   };
   return report;
 }
+
+// getNumberOfDepartments(allEmployees) { ... }: method shorthand = getNumberOfDepartments: function (allEmployees) { ... }.
+// Object.keys(): built-in that returns an array of keys.
