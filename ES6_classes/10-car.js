e7a862e2394d691aecd6bae86526d3ce58f5dfed
xl-c111 <10430@holbertonstudents.com>
2025-08-27 02:18:08 +1000

    Implement Car class with cloneCar method that returns a new instance of the class

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (
+      typeof brand !== 'string'
+      || typeof motor !== 'string'
+      || typeof color !== 'string'
+    ) {
+      throw new TypeError('brand, motor and color must be string');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
