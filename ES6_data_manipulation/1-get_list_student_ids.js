e4571f2869a235e2293e8a84afd7044db91db98b
xl-c111 <10430@holbertonstudents.com>
2025-08-27 13:09:33 +1000

    Add comments

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,6 @@ export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
     return [];
   }
+  // Using Destructuring: return array.map(({ id }) => id);
   return array.map((element) => element.id);
 }
