1c751eec1c3e9568cb0d0c749bc833dddab79d58
xl-c111 <10430@holbertonstudents.com>
2025-08-27 13:55:36 +1000

    Reduce

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+export default function getStudentIdsSum(studentslist) {
+  if (!Array.isArray(studentslist)) {
+    return 0;
+  }
+  return studentslist.reduce(
+    (accumulator, student) => accumulator + student.id,
+    0,
+  );
+}
