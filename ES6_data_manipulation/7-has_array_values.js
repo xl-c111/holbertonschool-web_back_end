3c7590ff25f8160133040921192cc760d63c7c57
xl-c111 <10430@holbertonstudents.com>
2025-08-28 01:19:33 +1000

    Add comment

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,9 @@
 export default function hasValuesFromArray(Set, array) {
   for (const value of array) {
     if (!Set.has(value)) {
-      return false;
+      return false; // as soon as one is missing, return false
     }
   }
-  return true;
+  return true; // all values were found
 }
+// return array.every(value) => Set.has(value);
