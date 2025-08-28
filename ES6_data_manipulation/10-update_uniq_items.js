93c35c1b826b5779bb7f06d32f7a7e01acac3557
xl-c111 <10430@holbertonstudents.com>
2025-08-28 20:18:34 +1000

    Add comments

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,12 @@
 export default function updateUniqueItems(map) {
+  // there is only 1 case that can't be processed, map is not a Map
   if (!(map instanceof Map)) {
-    throw new TypeError('map must be a Map');
+    throw new TypeError('Cannot process');
   }
   for (const [name, quantity] of map.entries()) {
     if (quantity === 1) {
       map.set(name, 100);
     }
   }
+  return map;
 }
