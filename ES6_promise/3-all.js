1a858c15a2ebf2d6b36ac3cf3c2a5163685490c7
xl-c111 <10430@holbertonstudents.com>
2025-08-29 01:18:19 +1000

    Add comments

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,20 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  // Promise.all() takes an array of promises as parameter.
+  return (
+    Promise.all([uploadPhoto(), createUser()])
+      // if all resolve, return array of results;
+      .then(([photo, user]) => {
+        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+      })
+      // if any reject, reject with error
+      .catch(() => {
+        console.log('Signup system offline');
+      })
+  );
 }
+
+// Promise.all([promise1, promise2, promise3])
+// .then((values) => {// values is an array of results})
+// .catch(error => {// first rejection error});
