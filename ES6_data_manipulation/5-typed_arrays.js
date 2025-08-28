68c757dc763bd052c977394595aa29b918b7a036
xl-c111 <10430@holbertonstudents.com>
2025-08-28 14:59:30 +1000

    setInt8 only writes data into buffer

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,17 +1,20 @@
 export default function createInt8TypedArray(length, position, value) {
   if (
-    typeof length !== 'number'
-    || typeof position !== 'number'
-    || typeof value !== 'number'
+    typeof length !== 'number' ||
+    typeof position !== 'number' ||
+    typeof value !== 'number'
   ) {
     throw new TypeError('length, position and value must be a number');
   }
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
+  // check buffer capacity
+  // if (position < 0 || position >= view.byteLength)
   if (position + 1 > view.byteLength) {
     throw new RangeError('Position outside range');
   }
+  // setInt8 only writes data into the buffer, not returns data
   view.setInt8(position, value);
   return view;
 }
