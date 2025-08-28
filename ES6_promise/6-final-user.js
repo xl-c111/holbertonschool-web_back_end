a39488e44958e4b5f3ab362a99e79a97a87a2d0e
xl-c111 <10430@holbertonstudents.com>
2025-08-29 02:32:10 +1000

    Add comments

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,21 +3,24 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   if (
-    typeof firstName !== 'string'
-    || typeof lastName !== 'string'
-    || typeof fileName !== 'string'
+    typeof firstName !== 'string' ||
+    typeof lastName !== 'string' ||
+    typeof fileName !== 'string'
   ) {
     return Promise.reject(
-      new TypeError('firstName, lastName, fileName should be string'),
+      new TypeError('firstName, lastName, fileName should be string')
     );
   }
 
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  return Promise.allSettled(promises).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return { status: result.status, value: result.value };
-    }
-    return { status: result.status, value: result.reason.toString() };
-  }));
+  // allSettled returns an array of results with either { status, value } or { status, reason }
+  // The checker expects { status, value }, even when rejected
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason.toString() };
+    })
+  );
 }
