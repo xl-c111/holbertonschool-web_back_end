93b658cc09dae72fabea8e0e2c4a5bf20eadd1b5
xl-c111 <10430@holbertonstudents.com>
2025-08-29 00:36:09 +1000

    Correct casing typo in response body

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'Success' };
+      return { status: 200, body: 'success' };
     })
     .catch(() => new Error());
 }
