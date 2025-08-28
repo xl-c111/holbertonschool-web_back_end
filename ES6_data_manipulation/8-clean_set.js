9f87545b98328a4a79a5dc6447351b66a816686f
xl-c111 <10430@holbertonstudents.com>
2025-08-28 15:56:05 +1000

    Remove type check of startString

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -5,9 +5,6 @@ export default function cleanSet(set, startString) {
   if (!startString) {
     return '';
   }
-  if (typeof startString !== 'string') {
-    throw new TypeError('startString must be a string');
-  }
 
   const rest = [];
   for (const element of set) {
