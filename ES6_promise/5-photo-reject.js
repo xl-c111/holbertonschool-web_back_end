5cd7e35604566c5e381d1e56458756c4bd288b46
xl-c111 <10430@holbertonstudents.com>
2025-08-29 03:37:45 +1000

    Promise.reject() return a Promise obj

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,9 @@
 export default function uploadPhoto(fileName) {
   if (typeof fileName !== 'string') {
+    // function expects to return a promise whether resolved or rejected
+    // throw new TypeError will throw synchronously, not return a promise
+    // Promise.reject(reason) creates a rejected Promise with the given reason
+    // returns a promise object that can be used with .then/.catch/await
     return Promise.reject(new TypeError('fileName must be a string'));
   }
   return Promise.reject(new Error(`${fileName} cannot be processed`));
