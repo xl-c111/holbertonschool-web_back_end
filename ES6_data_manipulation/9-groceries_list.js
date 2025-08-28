7b71c0e30dff8734bee34365bbd061f0197d8617
xl-c111 <10430@holbertonstudents.com>
2025-08-28 19:48:29 +1000

    Map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,22 @@
+const grocerieslist = [
+  'Apples',
+  10,
+  'Tomatoes',
+  10,
+  'Pasta',
+  1,
+  'Rice',
+  1,
+  'Banana',
+  5,
+];
+
+export default function groceriesList() {
+  const map = new Map();
+  for (let i = 0; i < grocerieslist.length; i += 2) {
+    const name = grocerieslist[i];
+    const quantity = grocerieslist[i + 1];
+    map.set(name, quantity);
+  }
+  return map;
+}
