2a6657626fa55e1719b1b6b6beddb65e34c005b7
xl-c111 <10430@holbertonstudents.com>
2025-08-30 23:36:50 +1000

    Add comments

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -1,11 +1,13 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  // weakMap.get() gets the previously count number
+  // weakMap.get(key) gets the previously count number
+  // will return the value for the key
   let count = weakMap.get(endpoint) || 0;
   count += 1;
 
-  // weakmap.set() saves the call count
+  // weakmap.set(key, value) saves the call count
+  // key must be an obj
   weakMap.set(endpoint, count);
 
   if (count >= 5) {
