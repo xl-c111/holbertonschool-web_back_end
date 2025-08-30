131bf9013251e4c1b34561886bc85e02c481e961
xl-c111 <10430@holbertonstudents.com>
2025-08-30 14:37:57 +1000

    Add commentd

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -14,12 +14,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   // allSettled returns an array of results with either { status, value } or { status, reason }
-  // The checker expects { status, value }, even when rejected
+  // .map() iterates over each result in the array and transform it into a new object
   return Promise.allSettled(promises).then((results) => results.map((result) => {
     if (result.status === 'fulfilled') {
       return { status: result.status, value: result.value };
     }
-    // String() can also convert any value to string
+    // reason is an Error object, .toString() ensures that the rejection reason is stored as a string
     return { status: result.status, value: result.reason.toString() };
   }));
 }
+
+/* Logic
+1, wait for all promises are settled, whether resolved or reject
+2, transform each result into a uniform object {status, value}
+   - if fullfilled, keep the value
+   - if rejected, convert reason into a string
+ */
