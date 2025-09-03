c6042202e33b7169a9e866510df265074cdc80a6
xl-c111 <10430@holbertonstudents.com>
2025-09-03 23:40:31 +1000

    Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 1245;
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+
+  response.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+module.exports = app;
