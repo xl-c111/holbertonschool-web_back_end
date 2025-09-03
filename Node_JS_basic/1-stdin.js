c646074966016959240d4c5dff9db2cefbf056f7
xl-c111 <10430@holbertonstudents.com>
2025-09-03 23:16:49 +1000

    Pass checker

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,31 @@
-process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (input) => {
-    console.log(`Your name is: ${input.trim()}`);
-    process.exit(0);
-  });
-} else {
-  process.stdin.on('data', (input) => {
-    console.log(`Your name is: ${input.trim()}`);
-  });
-  process.stdin.on('end', () => {
-    console.log('This important software is now closing');
-  });
-}
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+
+// process.stdin.setEncoding('utf8');
+
+// console.log('Welcome to Holberton School, what is your name?');
+
+// if (process.stdin.isTTY) {
+//   process.stdin.on('data', (input) => {
+//     console.log(`Your name is: ${input.trim()}`);
+//     process.exit(0);
+//   });
+// } else {
+//   process.stdin.on('data', (input) => {
+//     console.log(`Your name is: ${input.trim()}`);
+//   });
+//   process.stdin.on('end', () => {
+//     console.log('This important software is now closing');
+//   });
+// }
