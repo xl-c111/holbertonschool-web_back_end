a10ea834c29414d64f76b396a5db1a829de86666
xl-c111 <10430@holbertonstudents.com>
2025-09-04 15:24:57 +1000

    Create a small HTTP server using Express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
